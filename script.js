815028f503ff8b86f653ece865e1a734042060c5
Himanshu singh <76258924+himanshuxingh@users.noreply.github.com>
2023-08-31 00:03:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
-
 function genQR() {
     var goapi = "https://chart.googleapis.com/chart?chf=bg,s,65432100&cht=qr&chs=";
     var myimg = document.getElementById("img");
     var mytext = document.getElementById("qrtext").value;
     var mysize = document.getElementById("size").value;
 
-    if (mytext !== "" && mysize == "100") {
-        myimg.src = goapi + "100x100" + "&chl=" + mytext;
-        // https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl=hello
-        document.getElementById("download").style.display="block";
-    } else if (mytext !== "" && mysize == "150") {
-        myimg.src = goapi + "150x150" + "&chl=" + mytext;
-        document.getElementById("download").style.display="block";
-    } else if (mytext !== "" && mysize == "200") {
-        myimg.src = goapi + "200x200" + "&chl=" + mytext;
-        document.getElementById("download").style.display="block";
-    } else if (mytext !== "" && mysize == "250") {
-        myimg.src = goapi + "250x250" + "&chl=" + mytext;
-        document.getElementById("download").style.display="block";
-    } else if (mytext !== "" && mysize == "300") {
-        myimg.src = goapi + "300x300" + "&chl=" + mytext;
-        document.getElementById("download").style.display="block";
+    if (mytext !== "") {
+        var qrDataURL = goapi + mysize + "x" + mysize + "&chl=" + encodeURIComponent(mytext);
+        myimg.src = qrDataURL;
+        
+        // Convert the data URL to a blob and create an anchor element for download
+        fetch(qrDataURL)
+            .then(response => response.blob())
+            .then(blob => {
+                var downloadLink = document.getElementById("download");
+                downloadLink.href = URL.createObjectURL(blob);
+                downloadLink.style.display = "block";
+            })
+            .catch(error => {
+                console.error("Error fetching image:", error);
+            });
     } else {
         alert("Please Enter Text");
-
     }
-    
-    document.getElementById("download").href=myimg.src;   
-    
 }
-    
-
-
